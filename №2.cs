f0551b35a419d1a560be63a4d769da16a7955dc4
pl-mi <164654656+pl-mi@users.noreply.github.com>
2024-09-25 16:15:34 +0300

    Add files via upload

---
+++ №2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Laba02
+{
+    static void Main2()
+    {
+        Random rand = new Random();
+        int N = 10; // Кількість чисел у колекції
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < N; i++)
+        {
+            numbers.Add(rand.Next(15, 56));
+        }
+
+        Console.WriteLine("Оригінальна колекція:");
+        numbers.ForEach(n => Console.Write(n + " "));
+        Console.WriteLine();
+
+        // Знайти перше число кратне 3 або 5 і видалити його
+        int indexToRemove = numbers.FindIndex(n => n % 3 == 0 || n % 5 == 0);
+        if (indexToRemove != -1)
+        {
+            numbers.RemoveAt(indexToRemove);
+        }
+
+        Console.WriteLine("Колекція після видалення:");
+        numbers.ForEach(n => Console.Write(n + " "));
+    }
+}
---
+++ №3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Laba_02
+{
+    static void Main3()
+    {
+        Random rand = new Random();
+        int N = 10; // Кількість чисел у колекції
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < N; i++)
+        {
+            numbers.Add(rand.Next(10, 100));
+        }
+
+        Console.WriteLine("Оригінальна колекція:");
+        numbers.ForEach(n => Console.Write(n + " "));
+        Console.WriteLine();
+
+        List<int> multiplesOfThree = numbers.Where(n => n % 3 == 0).ToList();
+        List<int> nonMultiplesOfThree = numbers.Where(n => n % 3 != 0).ToList();
+
+        Console.WriteLine("Числа кратні 3:");
+        multiplesOfThree.ForEach(n => Console.Write(n + " "));
+        Console.WriteLine();
+
+        Console.WriteLine("Інші числа:");
+        nonMultiplesOfThree.ForEach(n => Console.Write(n + " "));
+    }
+}
